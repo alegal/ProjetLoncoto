bc7362b7a29c9b95d28d7d904c635f26dda9e61c
Louis BRETONNIERE <louisbretonniere@gmail.com>
2015-06-09 12:27:27 +0200

    Add template

---
+++ WebContent/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$(".dropdown-button").dropdown();
+	$('select').material_select();
+	$('.button-collapse').sideNav({
+		menuWidth: 240, // Default is 240
+    	edge: 'left', // Choose the horizontal origin
+    	closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+	$('.button-collapse-second').sideNav({
+		menuWidth: 240, // Default is 240
+    	edge: 'right', // Choose the horizontal origin
+    	closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+});
+
